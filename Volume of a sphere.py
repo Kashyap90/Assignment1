fbc15c25855f9e625c63065f42b799c73e889bcb
Kashyap90 <40736782+Kashyap90@users.noreply.github.com>
2018-07-05 01:36:11 +0530

    Add files via upload

---
+++ Volume of a sphere.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# The diameter of a sphere is 12cm and radius is half of the diameter, so the radius is 6
+pi=3.14
+r=6.0
+v=4.0/3.0*pi*r**3
+print('The volume of a sphere is',v)
+
